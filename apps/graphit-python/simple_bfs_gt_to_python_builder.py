30beb22984bd0c3abfc98d4133abc39521b47139
Eric Mei <mei.eric1@gmail.com>
2019-05-24 18:57:18 -0400

    c++ generation-- IOS functions mangled

--- apps/graphit-python/simple_bfs_gt_to_python_builder.py
+++ apps/graphit-python/simple_bfs_gt_to_python_builder.py
@@ -5,14 +5,24 @@ ffibuilder = FFI()
 
 basePath = os.path.dirname(__file__)
 filePath = os.path.abspath(os.path.join(basePath, "..", "..", "build/bin/simple_bfs.cpp"))
-compiledSimpleBFSFilePath = os.path.abspath(os.path.join(basePath, "..", "..", "build/bin/simple_bfs.cpp"))
+compiledSimpleBFSFilePath = os.path.abspath(os.path.join(basePath, "simple_bfs.a"))
+#bfs_include_path = os.path.abspath(join("include"))
+
+#cpp_path = path.Path("../../build/bin").abspath()
+
+#include_dirs = []
+#include_dirs.append(cpp_path.joinpath("include"))
+#include_dirs.append(cpp_build_path)
 
 
 runtimePath = os.path.abspath(os.path.join(basePath, "..", "..", "src/runtime_lib"))
 intrinsicsPath = os.path.abspath(os.path.join(basePath, "..", "..", "src/runtime_lib/intrinsics.h"))
 
-print("This is runtimepath")
-print(runtimePath)
+print("This is compiledSimpleBFSFilePath")
+print(compiledSimpleBFSFilePath)
+
+#print("This is bfs_include_path")
+#print(bfs_include_path)
 
 with open(filePath,'rb') as f:
     #externFunction = "r'''extern \"C\" {} {} {}'''".format('{', f.read(), '}')
@@ -22,17 +32,20 @@ with open(filePath,'rb') as f:
                           #r'''extern "C" {}{}{}'''.format('{', f.read(), '}'),
                           #f.read(),
                           r'''extern "C" {}''',
+                          language="c++",
+                          extra_objects=[compiledSimpleBFSFilePath],
                           include_dirs=[runtimePath],
                           source_extension = ".cpp",
-                          libraries=['simple_bfs'],
+                          libraries=["stdc++"],
+                          library_dirs=['simple_bfs'],
                           extra_compile_args = ["-std=c++11"]
 )
 
-ffibuilder.cdef(
-    """
-    struct run_bfs{};
-    """
-)
+#ffibuilder.cdef(
+#    """
+#    struct run_bfs{};
+#    """
+#)
 
 #with open(intrinsicsPath, "r") as f:
 #    ffibuilder.cdef(f.read())
